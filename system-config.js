a145966321db85ca3a6a9b97b5df30bb777a56a2
jasonhui <huihangkin@gmail.com>
2016-07-02 22:05:42 +0800

    first commit

---
+++ system-config.js
@@ -0,0 +1,73 @@
+// SystemJS configuration file, see links for more information
+// https://github.com/systemjs/systemjs
+// https://github.com/systemjs/systemjs/blob/master/docs/config-api.md
+/***********************************************************************************************
+ * User Configuration.
+ **********************************************************************************************/
+/** Map relative paths to URLs. */
+var map = {
+    'immutable': 'vendor/immutable/dist/immutable.js',
+    '@angular2-material': 'vendor/@angular2-material',
+};
+/** User packages configuration. */
+var packages = {
+    '@angular2-material/core': {
+        format: 'cjs',
+        defaultExtension: 'js',
+        main: 'core.js'
+    },
+    '@angular2-material/input': {
+        format: 'cjs',
+        defaultExtension: 'js',
+        main: 'input.js'
+    },
+    '@angular2-material/button': {
+        format: 'cjs',
+        defaultExtension: 'js',
+        main: 'button.js'
+    },
+    '@angular2-material/card': {
+        format: 'cjs',
+        defaultExtension: 'js',
+        main: 'card.js'
+    },
+};
+////////////////////////////////////////////////////////////////////////////////////////////////
+/***********************************************************************************************
+ * Everything underneath this line is managed by the CLI.
+ **********************************************************************************************/
+var barrels = [
+    // Angular specific barrels.
+    '@angular/core',
+    '@angular/common',
+    '@angular/compiler',
+    '@angular/forms',
+    '@angular/http',
+    '@angular/router',
+    '@angular/platform-browser',
+    '@angular/platform-browser-dynamic',
+    // Thirdparty barrels.
+    'rxjs',
+    // App specific barrels.
+    'app',
+    'app/shared',
+    'app/filters',
+    'app/talks',
+    'app/talks/talk',
+];
+var cliSystemConfigPackages = {};
+barrels.forEach(function (barrelName) {
+    cliSystemConfigPackages[barrelName] = { main: 'index' };
+});
+// Apply the CLI SystemJS configuration.
+System.config({
+    map: {
+        '@angular': 'vendor/@angular',
+        'rxjs': 'vendor/rxjs',
+        'main': 'main.js'
+    },
+    packages: cliSystemConfigPackages
+});
+// Apply the user's configuration.
+System.config({ map: map, packages: packages });
+//# sourceMappingURL=system-config.js.map
